aee56b8221b6c1ab61980334774eda9dcffcf0df
tessavermeulen07 <tessavermeulen@me.com>
2025-12-17 12:27:14 +0100

    opdracht 2.2 links op pagina werken, maar nog geen detailpagina's

--- src/pages/all-blogs/AllBlogs.jsx
+++ src/pages/all-blogs/AllBlogs.jsx
@@ -1,15 +1,18 @@
 import './AllBlogs.css';
 import posts from '../../constants/data.json'
-import {data} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 posts.shares = undefined;
 
 posts.author = undefined;
 
 function AllBlogs() {
-  console.log(posts)
+    console.log(posts)
 //console.log werkt
 
+    const {id} = useParams();
+    const idAsNumber = Number(id);
+    const currentPost = posts.find((posts) => posts.id === idAsNumber);
 
     // Notatie op deze manier:
     // De Smaken van Italië (Anna de Kok)
@@ -21,13 +24,14 @@ function AllBlogs() {
             <p>Aantal blogposts op deze website: {posts.length}</p>
             <ul className="list-container">
                 {posts.map((blog) => {
-                return <li key={blog.id}>
-                    <article className="list-item-container">
-                        <span>{blog.title} ({blog.author})</span>
-                        <span>{blog. comments} reacties - {blog.shares} keer gedeeld</span>
-                    </article>
-                </li>
-                }
+                        return <li key={blog.id}>
+                            <article className="list-item-container">
+                                <span><Link to="{`/blog/${blog.id}`}"
+                                             element={<AllBlogs/>}>{blog.title}</Link> ({blog.author})</span>
+                                <span>{blog.comments} reacties - {blog.shares} keer gedeeld</span>
+                            </article>
+                        </li>
+                    }
                 )}
             </ul>
         </>
@@ -35,29 +39,3 @@ function AllBlogs() {
 }
 
 export default AllBlogs;
-
-// <ul>
-//     {inventory.map((tv) => {
-//         return <li key={tv.type} className="most-sold image-most-sold">
-//             <img src={tv.sourceImg} alt={`Afbeelding van ${tv.name}`}/>
-//             <div className="most-sold-info">
-//                 <h3>{bestSoldTv(tv)}</h3>
-//                 <p className="numbers">{bestSoldTvPrice(tv)}</p>
-//                 <p>{bestSoldTvSize(tv)}</p>
-//
-//                 <ol className="list-box">
-//                     {tv.options.map((option) => {
-//                             if (option.applicable === true) {
-//                                 return <li key={option.name}><img src={check} alt="check icon"
-//                                                                   className="icon"/> {option.name}</li>
-//                             } else {
-//                                 return <li key={option.name}><img src={minus} alt="check icon"
-//                                                                   className="icon"/> {option.name}</li>
-//                             }
-//                         }
-//                     )}
-//                 </ol>
-//             </div>
-//         </li>
-//     })}
-// </ul>
